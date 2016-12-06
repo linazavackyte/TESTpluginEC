b5ac84e186d60ad550dc79e4dfde7fe15bfbbe5d
linazavackyte <lina.zavackyte@hashtages.com>
2016-12-06 13:32:35 +0000

    added order Info Helper

--- LICENSE.md
+++ LICENSE.md
@@ -1,5 +1,4 @@
-plentymarkets plugin hello world
-
+plentymarkets plugin payuponpickup
 Copyright © 2016 plentymarkets GmbH
 
 According to our dual licensing model, this program can be used either
---
+++ src/Helper/OrderInformationHelper.php
@@ -0,0 +1,82 @@
+<?php //strict
+
+    namespace EasyCredit\Helper;
+
+    use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+    use Plenty\Modules\Payment\Method\Models\PaymentMethod;
+
+    /**
+     * Class OrderInformationHelper
+     *
+     * @package EasyCredit\Helper
+     */
+    class OrderInformationHelper
+    {
+        /**
+         * @var $personData
+         */
+        private $personData;
+        
+        /**
+         * @var $billingAddress
+         */
+        private $billingAddress;
+        
+        /**
+         * @var $deliveryAddress
+         */
+        private $deliveryAddress;
+        
+        /**
+         * @var $amount
+         */
+        private $amount;
+        
+        /**
+         * OrderInformation constructor
+         * 
+         * @param $personData
+         * @param $billingAddress
+         * @param $deliveryAddress
+         * @param $amount
+         */
+        public function __construct()
+        {
+            $this->personData       = $personData;
+            $this->billingAdress    = $billingAddress;
+            $this->deliveryAddress  = $deliveryAddress;
+            $this->amount           = $amount;
+        }
+        
+        /**
+         * @return array(FirstName => string, LastName => string, Salutation => string)
+         */
+        public function getPersonData()
+        {
+            
+        }
+        
+        /**
+         * @return array(Street => string, City => string, Zip => int, CountryCode => string)
+         */
+        public function getBillingAddress()
+        {
+            $billingAddress = $this->sessionStorage->getPlugin()->getValue("billingAddressId");
+        }
+        
+        /**
+         * @return array(Street => string, City => string, Zip => int, CountryCode => string)
+         */
+        public function getDeliveryAddress()
+        {
+            
+        }
+        
+        /**
+         * @return float
+         */
+        public function getAmount()
+        {
+            
+        }
+    }
